65f4802d80c2556dba34e695441c3a7df1b21a38
Jeremy Moseley <jeremy@jeremymoseley.net>
2020-09-23 09:28:42 -0700

    Add a genreric hasura helper.

--- scripts/hasura-helper.sh
+++ scripts/hasura-helper.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
+set -ex
 
 cd ../hasura
+
 
 # TODO: Check for existence of hanja-config.json
 
@@ -9,4 +11,5 @@ export HASURA_GRAPHQL_ENDPOINT=$(cat ../hanja-config.prod.json | jq -r '.hasuraB
 export HASURA_GRAPHQL_ADMIN_SECRET=$(cat ../hanja-config.prod.json | jq -r '.adminSecret')
 # HASURA_GRAPHQL_ACTIONS_HANDLER_WEBHOOK_BASEURL=$(cat ../hanja-config.json | jq -r '.hasuraEndpoint')
 
-hasura console
+# Pass all the parameters
+hasura "${@:1}"
