4bdef6eed2a774ce1dc2264d46e5268441aa4253
Jeremy Moseley <jeremy@jeremymoseley.net>
2020-10-26 19:44:14 -0700

    Backwards compatible migration.

--- hasura/migrations/1603766384239_alter_table_public_users_add_column_email/up.sql
+++ hasura/migrations/1603766384239_alter_table_public_users_add_column_email/up.sql
@@ -1,1 +1,3 @@
-ALTER TABLE "public"."users" ADD COLUMN "email" citext NOT NULL UNIQUE;
+ALTER TABLE "public"."users" ADD COLUMN IF NOT EXISTS "email" citext NOT NULL UNIQUE;
+
+ALTER TABLE "public"."users" ALTER COLUMN "email" TYPE citext;
